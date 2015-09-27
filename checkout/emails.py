fccd8bf251b7f39adf06655989fb879490bdb7fd
Greg Brown <gregplaysguitar@gmail.com>
2015-09-28 12:53:01 +1300

    html emails for checkout

--- checkout/emails.py
+++ checkout/emails.py
@@ -1,13 +1,48 @@
+# -*- coding: utf-8 -*-
+
 from django.template.loader import render_to_string
-from django.core.mail import send_mail
 from django.conf import settings
+from django.contrib.sites.models import Site
+from django.core.mail import EmailMultiAlternatives
+
+
+TEMPLATE_DIR = 'checkout/emails/'
 
 
 def send_email_receipt(order):
-    subject = "Order received"
-    body = render_to_string("checkout/emails/receipt.txt", {
-        "order": order
+    send_email('receipt', [order.email], order=order)
+    send_email('notification', [t[1] for t in settings.CHECKOUT_MANAGERS],
+               order=order)
+
+
+def email_content(email_type, **context):
+    '''Return tuple of subject, text content and html content for a given email
+       type and context.'''
+
+    context.update({
+        'site': Site.objects.get_current(),
     })
-    send_mail(subject, body, settings.DEFAULT_FROM_EMAIL, [order.email])
-    send_mail(subject, body, settings.DEFAULT_FROM_EMAIL,
-              [t[0] for t in settings.CHECKOUT_MANAGERS])
+    subject = render_to_string(TEMPLATE_DIR + '%s_subject.txt' % email_type,
+                               context)
+    context['subject'] = subject
+    text_content = render_to_string(TEMPLATE_DIR + '%s.txt' % email_type,
+                                    context)
+
+    context['text_content'] = text_content
+    html_content = render_to_string(TEMPLATE_DIR + '%s.html' % email_type,
+                                    context)
+
+    return (subject.strip(), text_content.encode('ascii', 'ignore'),
+            html_content.encode('ascii', 'ignore'))
+
+
+def send_email(email_type, recipients, cc=[], bcc=[], **context_dict):
+    '''Send an email of a given type to email_address, using given context.'''
+
+    from_email = context_dict.get('from_email', settings.DEFAULT_FROM_EMAIL)
+    subject, text, html = email_content(email_type, **context_dict)
+
+    message = EmailMultiAlternatives(subject, text, from_email, recipients,
+                                     cc=cc, bcc=bcc)
+    message.attach_alternative(html, "text/html")
+    return message.send()

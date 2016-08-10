51579e9cb674602cb6bf965e36d4d7b2b547cd71
Paul Bright <paul@sons.co.nz>
2016-08-11 09:52:33 +1200

    Linter note

--- vouchers/api.py
+++ vouchers/api.py
@@ -1,3 +1,6 @@
+# Do not remove these imports, even though they are unused in this file, they
+# are necessary as this serves as the entry point to the application and will
+# therefore be imported from this file elsewhere.
 from .models import calculate_discounts, save_discounts
 from .admin import DiscountInline
 

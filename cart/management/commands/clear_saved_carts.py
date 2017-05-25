c7d06d0666087173f6062a3ec13ae238cd41f691
Paul Bright <paul@sons.co.nz>
2017-05-25 15:45:50 +1200

    Bugfix

--- cart/management/commands/clear_saved_carts.py
+++ cart/management/commands/clear_saved_carts.py
@@ -1,9 +1,18 @@
-from django.core.management.base import NoArgsCommand
+try:
+    from django.core.management.base import BaseCommand
 
+    class Command(BaseCommand):
+        help = "Deletes all SavedCart database entries."
 
-class Command(NoArgsCommand):
-    help = "Deletes all SavedCart database entries."
+        def handle(self, *args, **options):
+            from cart.models import SavedCart
+            SavedCart.objects.all().delete()
+except ImportError:
+    from django.core.management.base import NoArgsCommand
 
-    def handle_noargs(self, **options):
-        from cart.models import SavedCart
-        SavedCart.objects.all().delete()
+    class Command(NoArgsCommand):
+        help = "Deletes all SavedCart database entries."
+
+        def handle_noargs(self, **options):
+            from cart.models import SavedCart
+            SavedCart.objects.all().delete()

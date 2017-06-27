ab2a5b75ca3d7bfe31667bcba5ae6a54e82081c7
Greg Brown <greg@gregbrown.co.nz>
2017-06-27 13:08:46 +1200

    Add packaging

---
+++ setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf8
+
+import os
+
+from setuptools import setup
+
+# if there's a converted readme, use it, otherwise fall back to markdown
+if os.path.exists('README.rst'):
+    readme_path = 'README.rst'
+else:
+    readme_path = 'README.md'
+
+# avoid importing the module
+__version__ = None
+exec(open('shoptools/_version.py').read())
+
+setup(
+    name='django-shoptools',
+    version=__version__,
+    description='Recurring event tools for django',
+    long_description=open(readme_path).read(),
+    author='Greg Brown',
+    author_email='greg@sons.co.nz',
+    url='https://github.com/sonsandco/django-shoptools',
+    packages=['shoptools'],
+    license='BSD License',
+    zip_safe=False,
+    platforms='any',
+    install_requires=['Django>=1.8', 'django-countries>=4.0'],
+    include_package_data=True,
+    package_data={},
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Framework :: Django',
+    ],
+)

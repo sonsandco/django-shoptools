131cf585db8eeb1e8317d96ffd3afbd84e964478
Greg Brown <greg@gregbrown.co.nz>
2017-06-27 13:17:40 +1200

    tweak test setup

---
+++ shoptools/tests.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+
+
+class ShoptoolsTestCase(TestCase):
+    """Integration tests for shoptools apps. """
+
+    def setUp(self):
+        pass
+
+    def test_sample(self):
+        self.assertEqual(1, 1)

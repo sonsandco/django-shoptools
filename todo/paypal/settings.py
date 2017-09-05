a05487342fe71ee32654e8e4fcdfd12076255daf
Paul Bright <paul@sons.co.nz>
2017-09-05 12:29:19 +1200

    Remove old Paypal

--- todo/paypal/settings.py
+++
@@ -1,5 +0,0 @@
-from django.conf import settings
-
-# settings.DPS_TEMPLATE_ENGINE can be either 'django' or
-# 'jinja'/'jinja2' (aliases for the same engine)
-TEMPLATE_ENGINE = getattr(settings, 'PAYPAL_TEMPLATE_ENGINE', 'django')
